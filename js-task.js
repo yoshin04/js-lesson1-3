d69cc9ac951ec534d374206715dcf2a37c376fb6
yoshin04 <taigaby0411@gmail.com>
2020-11-25 16:37:52 +0900

    ループ処理&出力の修正

--- js-task.js
+++ js-task.js
@@ -5,9 +5,7 @@ const taskPush = (job) => tasks.push(job);
 console.log(title);
 tasks.forEach((task, index) => console.log(index, task));
 let job = prompt('タスクを入力して下さい。');
-while (job) {
-  taskPush(job);
-  tasks.forEach((task, index) => console.log(index, task));
-  alert('新しいタスクを入力しました。');
-  job = prompt('タスクを入力して下さい');
-}
+taskPush(job);
+console.log(title);
+tasks.forEach((task, index) => console.log(index, task));
+alert('新しいタスクを入力しました。');
